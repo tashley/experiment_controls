35c4a2c4209be9521f4eccc5df8679f6f2728e9b
Tom Ashley <tashley22@gmail.com>
2018-05-28 14:12:09 -0600

    Flash controls

--- flash_controls.py
+++ flash_controls.py
@@ -4,3 +4,19 @@ Collection of tools for controlling camera flash trigger using a
 Numato Labs 8 channel USB GPIO board
 
 """
+
+import serial
+from time import sleep
+
+#%%
+
+ser = serial.Serial('COM6', 19200, timeout=1)
+
+
+#%%
+
+ser.write("gpio set 0\r\n".encode())
+sleep(0.5)
+ser.write('gpio clear 0\r\n'.encode())
+ #%%
+ser.close()
