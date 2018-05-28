2395f3df03cdf139b8b857f6bf52c3e3c7032b1c
Tom Ashley <tashley22@gmail.com>
2018-05-28 13:56:35 -0600

    added docstrings

---
+++ flash_controls.py
@@ -0,0 +1,6 @@
+"""
+
+Collection of tools for controlling camera flash trigger using a
+Numato Labs 8 channel USB GPIO board
+
+"""
---
+++ motor_controls.py
@@ -0,0 +1,6 @@
+"""
+
+Collection of tools used to control a linear actuator powered by an Mdrive23
+stepper motor
+
+"""
