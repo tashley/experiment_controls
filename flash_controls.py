f868bd82b4a23e645b930b36f5234b4795d9e710
Tom Ashley <tashley22@gmail.com>
2018-05-28 14:48:53 -0600

    flash_controls complete

--- flash_controls.py
+++ flash_controls.py
@@ -10,13 +10,19 @@ from time import sleep
 
 #%%
 
-ser = serial.Serial('COM6', 19200, timeout=1)
+class Flash(object):
+      def __init__(self, port):
+            """"Open flash connection"""
+            self.con = serial.Serial(port, 19200, timeout=1)
+            sleep(1)
+            self.con.write("gpio clear 0\r\n".encode())
+      
+      def fire(self):
+            """ Fire flash and delay 0.1 seconds"""
+            self.con.write("gpio set 0\r\n".encode())
+            self.con.write("gpio clear 0\r\n".encode())
 
-
-#%%
-
-ser.write("gpio set 0\r\n".encode())
-sleep(0.5)
-ser.write('gpio clear 0\r\n'.encode())
- #%%
-ser.close()
+      def close(self):
+            """ Close flash connection """
+            self.con.close()
+            sleep(1)
--- motor_controls.py
+++ motor_controls.py
@@ -4,3 +4,29 @@ Collection of tools used to control a linear actuator powered by an Mdrive23
 stepper motor
 
 """
+
+import serial
+from time import sleep
+
+#%%
+
+ser = serial.Serial('COM3', 9600, timeout=0, writeTimeout=0)
+
+#%%
+ser.write('mr -8000\r\n'.encode())
+
+
+#%%
+ser.close()
+
+
+
+# motor = Motor('COM3')
+# motor.initialize()
+
+# flash = Flash('COM5')
+
+
+# for i in nits:
+      # flash.fire()
+      # motor.run_profile(upspeed, downspeed)
